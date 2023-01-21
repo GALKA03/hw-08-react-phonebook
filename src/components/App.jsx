fc47a46c9608ac1b8e6c1ed11155def9cbde7c6c
Galina Matvienko <galyagmgm@gmail.com>
2023-01-21 13:12:20 +0100

    kjnk
    knkn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import HomePage from 'pages/HomePage/HomePage';
 import ContactsPage from 'pages/ContactsPage/ContactsPage';
 import RegisterPage from 'pages/RegisterPage/RegisterPage';
@@ -9,20 +9,29 @@ import Layout from 'components/Layout/Layout';
 import { fetchCurrentUser } from 'redux/auth/auth-operations';
 import PrivateRoute from './HOCs/PrivateRoute';
 import PublicRoute from './HOCs/PublicRoute';
+import { selectIsFetchingCurrentUser } from 'redux/auth/authSelectors';
+
+
+
+
 
 export const App = () => {
   const dispatch = useDispatch();
+const isFechingCurrentUser = useSelector(selectIsFetchingCurrentUser)
+
   useEffect(() => {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
+  
   return (
     <>
-      <Routes>
+      {!isFechingCurrentUser && ( 
+<Routes>
         <Route path="/" element={<Layout />}>
           <Route
             path="/"
             element={
-              <PublicRoute restricted>
+              <PublicRoute>
                 <HomePage />
               </PublicRoute>
             }
@@ -53,6 +62,8 @@ export const App = () => {
           />
         </Route>
       </Routes>
+      )}
+      
     </>
   );
 };
