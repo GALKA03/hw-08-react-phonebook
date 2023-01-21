8905abcb28051c6d73e614633cf3bc17f131818c
Galina Matvienko <galyagmgm@gmail.com>
2023-01-21 13:01:07 +0100

    Update contacts-operations.js

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -1,12 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL ='https://63c00004e262345656f53441.mockapi.io/contacts/v1';
+//axios.defaults.baseURL ='https://63c00004e262345656f53441.mockapi.io/contacts/v1';
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async (_, { rejectWithValue }) => {
     try {
-        const { data } = await axios('/contacts');
+        const { data } = await axios('https://63c00004e262345656f53441.mockapi.io/contacts/v1/contacts');
       return data;
     } catch (error) {
       return rejectWithValue(error);
@@ -17,7 +17,7 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (id, { rejectWithValue }) => {
     try {
-      await axios.delete(`/contacts/${id}`);
+      await axios.delete(`https://63c00004e262345656f53441.mockapi.io/contacts/v1/contacts/${id}`);
       return id;
     } catch (error) {
       return rejectWithValue(error);
@@ -28,7 +28,7 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, { rejectWithValue }) => {
     try {
-      const { data } = await axios.post(`/contacts`, contact);
+      const { data } = await axios.post(`https://63c00004e262345656f53441.mockapi.io/contacts/v1/contacts`, contact);
       return data;
     } catch (error) {
       return rejectWithValue(error);
